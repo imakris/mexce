0099967fbb286f98a49226ace31dab2f2abcb7db
Ioannis Makris <imak@imak.gr>
2025-10-24 04:12:12 +0200

    Optimize constant-exponent pow codegen

--- mexce.h
+++ mexce.h
@@ -868,88 +868,65 @@ void pow_optimizer(elist_it_t it, evaluator* ev, elist_t* elist)
         double r_d = round(v_d);
         double a_d = abs(v_d);
 
-        bool matched = true;
+        bool matched = false;
         mexce_charstream s;
 
         // a special case, that the exponent is 0.5
         if (v_d == 0.5) {
             s < 0xd9 < 0xfa;                // fsqrt
+            matched = true;
         }
-        else
-        if (r_d == v_d && a_d <= 65536.0) {
+        else if (r_d == v_d && a_d <= 65536.0) {
+            matched = true;
 
-            // find the closest power of two
-            uint32_t npo2 = (uint32_t)a_d;
-            npo2--;
-            npo2 |= npo2 >> 1;
-            npo2 |= npo2 >> 2;
-            npo2 |= npo2 >> 4;
-            npo2 |= npo2 >> 8;
-            npo2 |= npo2 >> 16;
-            npo2++;
-            npo2>>=1;
+            const bool negative_exp = v_d < 0.0;
+            const uint32_t exp = static_cast<uint32_t>(a_d);
 
-            double diff_high = npo2*2 - a_d;
-            double diff_low  = a_d - npo2;
-
-            if (a_d == 0.0) {
+            if (exp == 0) {
                 s < 0xdd < 0xd8             // fstp st(0)
                   < 0xd9 < 0xe8;            // fld1
             }
-            else
-            if (a_d == 1.0) {
-                // do nothing
+            else if (exp == 1) {
+                if (negative_exp) {
+                    s < 0xd9 < 0xe8;        // fld1
+                    s < 0xde < 0xf1;        // fdivrp      st(1), st
+                }
             }
-            else
-            if (diff_high < 28 &&  diff_low < 28) {
-                if (diff_high && diff_low) {
-                    // the exponent is not an exact po2, thus we will have to multiply
-                    // or divide to get to the result, thus we keep the base in st(1)
-                    s < 0xd9 < 0xc0;        // fld  st(0)
-                }
-                while (npo2 >>= 1) {        // multiply to reach npo2
-                    s < 0xdc < 0xc8;        // fmul st(0)
-                }
-
-                if (diff_high < diff_low) {
-                    s < 0xdc < 0xc8;        // fmul st(0)
-
-                    // divide as many times as the difference
-                    // and then get rid of the temporary
-                    if (diff_high > 0.0) {
-                        while (--diff_high) {
-                            s < 0xd8 < 0xf1;    // fdivr  st(0), st(1)
-                        }
-                        s < 0xde < 0xf1;        // fdivrp st(1), st(0)
-                    }
-                }
-                else {
-                    // multiply as many times as the difference
-                    // and then get rid of the temporary
-                    if (diff_low > 0) {
-                        while (--diff_low) {
-                            s < 0xd8 < 0xc9;    // fmul  st(0), st(1)
-                        }
-                        s < 0xde < 0xc9;        // fmulp st(1), st(0)
-                    }
-                }
-
-                if (diff_high &&  diff_low) {
-                    // now we get rid of the temporary we used earlier
-                    s < 0xde < 0xc9;            // fmulp st(1), st(0)
+            else if (exp == 2) {
+                s < 0xdc < 0xc8;            // fmul st(0), st(0)
+                if (negative_exp) {
+                    s < 0xd9 < 0xe8;        // fld1
+                    s < 0xde < 0xf1;        // fdivrp      st(1), st
                 }
             }
             else {
-                matched = false;
-            }
+                uint32_t remaining = exp;
 
-            if (matched && v_d < 0) {
-                s < 0xd9 < 0xe8                 // fld1
-                  < 0xde < 0xf1;                // fdivrp  st(1),st   // inverse
+                s < 0xd9 < 0xe8;            // fld1 (result)
+
+                while (remaining) {
+                    if (remaining & 1u) {
+                        s < 0xd8 < 0xc9;    // fmul st(0), st(1)
+                    }
+
+                    remaining >>= 1u;
+                    if (!remaining) {
+                        break;
+                    }
+
+                    s < 0xd9 < 0xc9;        // fxch st(1)
+                    s < 0xdc < 0xc8;        // fmul st(0), st(0)
+                    s < 0xd9 < 0xc9;        // fxch st(1)
+                }
+
+                s < 0xd9 < 0xc9;            // fxch st(1)
+                s < 0xdd < 0xd8;            // fstp st(0)
+
+                if (negative_exp) {
+                    s < 0xd9 < 0xe8;        // fld1
+                    s < 0xde < 0xf1;        // fdivrp      st(1), st
+                }
             }
-        }
-        else {
-            matched = false;
         }
 
         if (!matched) {
